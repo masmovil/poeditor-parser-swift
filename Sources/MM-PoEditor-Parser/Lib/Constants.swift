640d6184b74f8d779976796e1cd5fb35ef4620d6
Sebastián Varela <sebastian.varela@masmovil.com>
2024-10-01 18:51:05 +0200

    Prepare v2.0.0

--- Sources/MM-PoEditor-Parser/Lib/Constants.swift
+++ Sources/MM-PoEditor-Parser/Lib/Constants.swift
@@ -21,7 +21,7 @@ enum POEConstants {
 
     """
 
-    static let version = "1.9.3"
+    static let version = "2.0.0"
 
     static func literalsStructHeader(name: String) -> String { "public struct \(name) {\n" }
     static func literalsStructStaticTableName(name: String?) -> String {
