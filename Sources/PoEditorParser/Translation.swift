e3e97e7de9fbbf5f5d39d5160dbf389bd1190a8e
David Martinez <daviwiki@users.noreply.github.com>
2026-01-16 15:20:00 +0100

    feat: Add new support to Struct migration (#7)
    Co-authored-by: Sebastián Varela <sebastian.varela@masmovil.com>

--- Sources/PoEditorParser/Translation.swift
+++ Sources/PoEditorParser/Translation.swift
@@ -36,13 +36,13 @@ public struct Translation: Comparable {
 
     public var swiftEnumCaseCode: String {
         if variables.isEmpty {
-            return "\tcase \(prettyKey)"
+            return "\tcase \(keySafeForCodeMethod)"
         }
         let parameters = variables
             .map { $0.type.swiftParameter(key: $0.parameterKey) }
             .joined(separator: ", ")
 
-        return "\tcase \(prettyKey)(\(parameters))"
+        return "\tcase \(keySafeForCodeMethod)(\(parameters))"
     }
 
     public var swiftEnumCaseForValue: String {
@@ -54,22 +54,29 @@ public struct Translation: Comparable {
     }
 
     public var swiftEnumCaseForKey: String {
-        "\t\tcase .\(prettyKey): return \"\(key)\""
+        "\t\tcase .\(keySafeForCodeMethod): return \"\(key)\""
     }
 
-    private var prettyKey: String {
+    private var keySafeForCodeMethod: String {
         switch keysFormat {
         case .upperCamelCase:
-            return key.capitalized.replacingOccurrences(of: "_", with: "")
+            return key.capitalized
+                .replacingOccurrences(of: "_", with: "")
+                .replacingOccurrences(of: "[", with: "")
+                .replacingOccurrences(of: "]", with: "")
+                .replacingOccurrences(of: "|", with: "")
 
         case .lowerCamelCase:
             return (key.prefix(1).lowercased() + key.capitalized.dropFirst())
                 .replacingOccurrences(of: "_", with: "")
+                .replacingOccurrences(of: "[", with: "")
+                .replacingOccurrences(of: "]", with: "")
+                .replacingOccurrences(of: "|", with: "")
         }
     }
 
     private func generateEnumCaseWithoutVariables() -> String {
-        "\t\tcase .\(prettyKey): return value"
+        "\t\tcase .\(keySafeForCodeMethod): return value"
     }
 
     private func generateEnumCaseWithVariables() -> String {
@@ -86,45 +93,98 @@ public struct Translation: Comparable {
                 return ".replacingOccurrences(of: \"{{\(variable.parameterKey)}}\", with: \(variable.parameterKey.snakeCased()))"
             }
             .joined(separator: "\n\t\t\t")
-        return "\t\tcase .\(prettyKey)(\(parameters)): return value\n\t\t\t\(localizedArguments)"
+        return "\t\tcase .\(keySafeForCodeMethod)(\(parameters)): return value\n\t\t\t\(localizedArguments)"
     }
 
     private func generateFuncWithoutVariables() -> String {
         /*
-         static var Welcome: String {
-         return NSLocalizedString()
-         }
+         static let key: StringsUIKey = StringsUIKey(key: "key_id")
          */
-        "\tpublic static var \(prettyKey): String {\n\t\treturn NSLocalizedString(\"\(key)\", tableName: \(typeName).tableName, comment: \"\")\n\t}\n"
+        """
+        \(commentKey(maxLength: 700))
+            static let \(keySafeForCodeMethod): \(typeName) = \(typeName)(key: \"\(key)\")
+        """
     }
 
     private func generateFuncWithVariables() -> String {
         /*
-         static func ReadBooksKey(readNumber: Int) -> String {
-         return ""
+         static func key(parameter1: String) -> StringsUIKey {
+         .init(key: "key", parameters: ["parameter1": parameter1])
          }
          */
         let parameters = variables
             .map { $0.type.swiftParameter(key: $0.parameterKey) }
             .joined(separator: ", ")
+
         let localizedArguments = variables
             .map { variable in
-                if variable.type != .textual {
-                    let value = "String(format: \"\(variable.type.localizedRepresentation)\", \(variable.parameterKey.snakeCased()))"
-                    return ".replacingOccurrences(of: \"{{\(variable.parameterKey)}}\", with: \(value))"
-                }
-
-                return ".replacingOccurrences(of: \"{{\(variable.parameterKey)}}\", with: \(variable.parameterKey.snakeCased()))"
+                (
+                    key: "\"\(variable.parameterKey)\"",
+                    value: variable.toParameterValue()
+                )
             }
-            .joined(separator: "\n\t\t\t")
-        return "\tpublic static func \(prettyKey)(\(parameters)) -> String {\n\t\treturn NSLocalizedString(\"\(key)\", tableName: \(typeName).tableName, comment: \"\")\n\t\t\t\(localizedArguments)\n\t}\n"
+
+        var localizedArgumentsString: String {
+            var result: [String] = []
+            for (key, value) in localizedArguments {
+                result.append("\(key): \(value)")
+            }
+            return "[\(result.joined(separator: ", "))]"
+        }
+
+        return """
+        \(commentKey(maxLength: 700))
+            static func \(keySafeForCodeMethod)(\(parameters)) -> \(typeName) {
+                .init(key: \"\(key)\", parameters: \(localizedArgumentsString))
+            }
+        """
     }
 
     public static func < (lhs: Translation, rhs: Translation) -> Bool {
-        lhs.prettyKey < rhs.prettyKey
+        lhs.keySafeForCodeMethod < rhs.keySafeForCodeMethod
     }
 
     public static func == (lhs: Translation, rhs: Translation) -> Bool {
-        lhs.prettyKey == rhs.prettyKey
+        lhs.keySafeForCodeMethod == rhs.keySafeForCodeMethod
+    }
+
+    private func commentKey(maxLength: Int) -> String {
+        let truncatedValue: String = {
+            if value.count <= maxLength {
+                return value
+            }
+            let truncated = String(value.prefix(maxLength - 3))
+            return "\(truncated)..."
+        }()
+
+        if variables.isEmpty {
+            return """
+                /// Returns Literal instance for `\(key)` key.
+                /// - Example: \(truncatedValue)
+            """
+        } else {
+            let parameters = variables
+                .map { "    ///     - \($0.parameterKey): Replace *{{\($0.parameterKey)}}* with the given value in \($0.type.swiftType)" }
+                .joined(separator: "\n")
+            return """
+                /// Returns Literal instance for `\(key)` key.
+                /// - Parameters:
+            \(parameters)
+                /// - Example: \(truncatedValue)
+            """
+        }
+    }
+}
+
+private extension Variable {
+    func toParameterValue() -> String {
+        switch type {
+        case .textual:
+            return parameterKey.snakeCased()
+
+        case .numeric:
+            let value = "String(format: \"\(type.localizedRepresentation)\", \(parameterKey.snakeCased()))"
+            return value
+        }
     }
 }
